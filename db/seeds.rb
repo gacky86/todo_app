f18b280e862a4b297c9bcca2a79861fa94f456ad
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-01-17 11:48:36 +0100

    add Todo model and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+SAMPLE_TODOS = [
+  { name: 'Going around the world' },
+  { name: 'Graduating from college' },
+  { name: 'Publishing a book' }
+]
+
+SAMPLE_TODOS.each { |todo| Todo.create(todo) }
