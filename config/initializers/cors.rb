86b5341576e16df2da57306e07d855ffbb4e6070
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-01-17 14:09:49 +0100

    create components

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001' # ReactサーバーのURL
+    origins 'http://localhost:5173' # ReactサーバーのURL
     resource '*', # すべてのエンドポイントに適用
       headers: :any, # すべてのヘッダーを許可
       methods: [:get, :post, :put, :patch, :delete, :options, :head] # 許可するHTTPメソッドを定義
